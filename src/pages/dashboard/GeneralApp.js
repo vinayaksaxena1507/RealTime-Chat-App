bf988c14348af1c4145532e1a329205bbc02f22c
Vinayak Saxena <vinayaksaxena159@gmail.com>
2023-05-04 22:28:02 +0530

    Implement Chat Area design like header and footer

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,13 +1,32 @@
 import React from "react";
 import Chats from "./Chats";
+import { Box, Stack } from "@mui/material";
+import Conversation from "../../components/Conversation";
+import { useTheme } from "@mui/material/styles";
+
 
 const GeneralApp = () => {
-
+  const theme = useTheme();
   return (
-    <>
+    <Stack direction="row" sx={{ width: "100%" }}>
       {/* Chats */}
-      <Chats/>
-    </>
+      <Chats />
+
+      {/* Conversation */}
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px )",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#F8FAFF"
+              : theme.palette.background.default,
+        }}
+      >
+        {/* Conversation */}
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
